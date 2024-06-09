bee1c10f155aacdc6c0ef71d06d3e1a7c6afc6b6
Fethisensei <fetam62@gmail.com>
2024-06-09 21:55:24 +0200

    doc(app.js): add app.js file in the dist folder

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,15 @@
-console.log("hello,world");
+/*
+- créer 6 variables, btnAttack, btnSpecialAttack, btnheal, btnGiveUp, healthPlayer, healthMonster,
+- j'assigne à chaque variable une fonction :
+- btnAttack = fonction (diminuer le chiffre qui est à l'intérieur de healthmonster, mais diminue aussi le nombre qui est à l'intérieur de healthplayer);
+- btnSpecialAttack = fonction (diminuer le chiffre qui est à l'intérieur de healthmonster, mais diminue aussi le nombre qui est à l'intérieur de healthplayer);
+- btnHeal = fonction (augmente le nombre qui est à l'intérieur de healthplayer s'il est inférieur à 100)
+- btnGiveUp = fonction (envoie une alerte nous indiquant que nous avons abandonné la partie)
+*/
+
+
+
+
+
+
+
